14307b2fcd1e2543921b975aca9047de83377dc6
B80B1 <tjanders.62@gmail.com>
2024-02-28 11:46:50 -0600

    Light Editing of Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCv/TPDetect.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCv/TPDetect.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode.OpenCv;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
@@ -14,91 +17,39 @@ import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 
-@Autonomous
-public class TPDetect extends OpMode {
+@Config
+public class TPDetect extends OpenCvPipeline {
+    public static boolean Detect_Blue = true;
+    public static double MinimumVal = 100;
+    public static double MaximumVal = 255;
+    public static double MinimumBHue = 100;
+    public static double MaximumBHue = 170;
+    public static double MinimumRLHue = 0;
+    public static double MaximumRLHue = 30;
+    public static double MinimumRHHue = 160;
+    public static double MaximumRHHue = 255;
 
-    OpenCvCamera webcam1 = null;
-    static final int STREAM_WIDTH = 1280;
-    static final int STREAM_HEIGHT = 720;
-
-    @Override
-    public void init(){
-
-        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webcam 1");
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "Id", hardwareMap.appContext.getPackageName());
-        webcam1 = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
-
-        webcam1.setPipeline(new Blue_Pipeline());
-
-        webcam1.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
-            @Override
-            public void onOpened() {
-                webcam1.startStreaming(1280,720, OpenCvCameraRotation.UPRIGHT);
-            }
-
-            @Override
-            public void onError(int errorCode) {
-
-            }
-        });
-
-
+    Telemetry telemetry;
+    Mat mat = new Mat();
+    public enum Location {
+        Right,
+        Left,
+        Middle
+    }
+    private Location location;
+    static final Rect LeftArea = new Rect(new Point(10,100), new Point(105,200));
+    static final Rect RightArea = new Rect(new Point(10, 200), new Point(205, 200));
+    static final Rect MiddleArea = new Rect(new Point(220,100), new Point(310,200));
+    public CenterStageCVDetection(Telemetry t){
+        telemetry = t;
     }
 
-    class
-    Blue_Pipeline extends OpenCvPipeline {
-        Mat YCbCr = new Mat();
-        Mat RightCrop;
-        Mat MidCrop;
-        Mat LeftCrop;
-        double RAvgFin;
-        double LAvgFin;
-        double MAvgFin;
-        Mat outPut = new Mat();
-        Scalar rectColor = new Scalar(0.0, 0.0, 255.0);
-        @Override
-        public Mat processFrame(Mat input) {
-            Imgproc.cvtColor(input,YCbCr,Imgproc.COLOR_RGB2YCrCb);
-            telemetry.addLine("Pipeline running");
-
-            Rect LeftRec = new Rect(1, 1, 425, 719);
-            Rect MidRec = new Rect(426, 1, 425, 719);
-            Rect RightRec = new Rect(720, 1, 425, 719);
-
-            input.copyTo(outPut);
-            Imgproc.rectangle(outPut, LeftRec, rectColor, 2);
-            Imgproc.rectangle(outPut, RightRec, rectColor, 2);
-            Imgproc.rectangle(outPut, MidRec, rectColor, 2);
-
-            RightCrop = YCbCr.submat(RightRec);
-            LeftCrop = YCbCr.submat(LeftRec);
-            MidCrop = YCbCr.submat(MidRec);
-
-            Core.extractChannel(LeftCrop, LeftCrop, 2);
-            Core.extractChannel(RightCrop, RightCrop, 2);
-            Core.extractChannel(MidCrop, MidCrop, 2);
-
-            Scalar LAvg = Core.mean(LeftCrop);
-            Scalar RAvg = Core.mean(RightCrop);
-            Scalar MAvg = Core.mean(MidCrop);
-
-            LAvgFin = LAvg.val[0];
-            RAvgFin = RAvg.val[0];
-            MAvgFin = MAvg.val[0];
-
-            if ((RAvgFin == MAvgFin) && LAvgFin != MAvgFin) {
-                    telemetry.addLine("Left");
-            } else if ((LAvgFin == MAvgFin) && RAvgFin != MAvgFin) {
-                    telemetry.addLine("Right");
-            } else {
-                    telemetry.addLine("Middle");
-            }
-            YCbCr.release();
-            return(outPut);
-        }
-    }
     @Override
-    public void loop(){
+    public Mat processFrame(Mat input){
+        Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
 
+        Scalar MinB = new Scalar(MinimumBHue,MinimumVal,MinimumVal);
+        Scalar MaxB = new Scalar(MaximumBHue,MaximumVal,MaximumVal);
     }
 }
+
