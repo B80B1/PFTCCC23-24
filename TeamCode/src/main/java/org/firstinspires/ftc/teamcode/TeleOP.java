3b2be475d1658b6f06935bf1bc8b8e3ce2dc43b9
B80B1 <tjanders.62@gmail.com>
2024-04-03 11:23:33 -0500

    Light Editing of Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
@@ -94,9 +94,9 @@ public class TeleOP extends LinearOpMode {
             motor7.setPower(m7Power);
 
             if (gamepad2.right_bumper) {
-                C.setPosition(-0.4);
-            } else {
                 C.setPosition(0);
+            } else {
+                C.setPosition(0.4);
             } if (gamepad2.dpad_left) {
                 PL.setPosition(0);
             } else if (gamepad2.dpad_down){
