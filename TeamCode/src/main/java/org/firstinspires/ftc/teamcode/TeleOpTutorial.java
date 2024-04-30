b2d4f063c81c1805815bccbf29854939d4bae669
jdjiv <jdonjonesiv@gmail.com>
2024-04-30 11:39:01 -0500

    E

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTutorial.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTutorial.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "TeleOpTutorial")
 public class TeleOpTutorial extends LinearOpMode
@@ -12,12 +13,18 @@ public class TeleOpTutorial extends LinearOpMode
     private DcMotor rightFront;
     private DcMotor leftBack;
     private DcMotor rightBack;
+
+    private Servo bluearm
+    private Servo orangearm
     @Override
     public void runOpMode() throws InterruptedException {
         leftFront = hardwareMap.dcMotor.get("leftFront");
         rightFront = hardwareMap.dcMotor.get("rightFront");
         leftBack = hardwareMap.dcMotor.get("leftBack");
         rightBack = hardwareMap.dcMotor.get("rightBack");
+        bluearm = hardwareMap.servo.get("bluearm")
+        orangearm = hardwareMap.servo.get("orangearm")
+
 
         leftBack.setDirection(DcMotor.Direction.REVERSE);
         leftFront.setDirection(DcMotor.Direction.REVERSE);
@@ -26,10 +33,15 @@ public class TeleOpTutorial extends LinearOpMode
 
         while(opModeIsActive()) {
 
+            bluearm.setPosition(-gamepad1.right_bumper);
+            orangearm.setPosition(-gamepad1.right_bumper);
+
             leftFront.setPower(-gamepad1.left_stick_y);
             rightFront.setPower(-gamepad1.right_stick_y);
             leftBack.setPower(-gamepad1.left_stick_y);
             rightBack.setPower(-gamepad1.right_stick_y);
+
+            
 
             idle();
         }
