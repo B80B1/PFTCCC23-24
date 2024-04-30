1640117b55fc6bdaf46b75699c78d5345d2f8269
jdjiv <jdonjonesiv@gmail.com>
2024-04-30 11:48:45 -0500

    Update TeleOpTutorial.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTutorial.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTutorial.java
@@ -7,23 +7,23 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "TeleOpTutorial")
-public class TeleOpTutorial extends LinearOpMode
-{
+public class TeleOpTutorial extends LinearOpMode {
     private DcMotor leftFront;
     private DcMotor rightFront;
     private DcMotor leftBack;
     private DcMotor rightBack;
 
-    private Servo bluearm
-    private Servo orangearm
+    private Servo bluearm;
+    private Servo orangearm;
+
     @Override
     public void runOpMode() throws InterruptedException {
         leftFront = hardwareMap.dcMotor.get("leftFront");
         rightFront = hardwareMap.dcMotor.get("rightFront");
         leftBack = hardwareMap.dcMotor.get("leftBack");
         rightBack = hardwareMap.dcMotor.get("rightBack");
-        bluearm = hardwareMap.servo.get("bluearm")
-        orangearm = hardwareMap.servo.get("orangearm")
+        bluearm = hardwareMap.servo.get("bluearm");
+        orangearm = hardwareMap.servo.get("orangearm");
 
 
         leftBack.setDirection(DcMotor.Direction.REVERSE);
@@ -31,19 +31,24 @@ public class TeleOpTutorial extends LinearOpMode
 
         waitForStart();
 
-        while(opModeIsActive()) {
-
-            bluearm.setPosition(-gamepad1.right_bumper);
-            orangearm.setPosition(-gamepad1.right_bumper);
+        while (opModeIsActive()) {
 
             leftFront.setPower(-gamepad1.left_stick_y);
             rightFront.setPower(-gamepad1.right_stick_y);
             leftBack.setPower(-gamepad1.left_stick_y);
             rightBack.setPower(-gamepad1.right_stick_y);
 
-            
-
-            idle();
+            if (gamepad1.right_bumper) {
+                bluearm.setPosition(0.5);
+            } else {
+                bluearm.setPosition(0.0);
+                if (gamepad1.right_bumper) {
+                    orangearm.setPosition(-0.5);
+                } else {
+                    orangearm.setPosition(0.0);
+                    idle();
+                }
+            }
         }
     }
 }
